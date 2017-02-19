df4edb236a7bf7d8011ebcfb01f50fe0f00199a5
Stanislaw Babkin <stalissss@outlook.com>
2017-02-19 18:57:07 +0300

    Fixed animation and control improvement

--- Game/js/Game.js
+++ Game/js/Game.js
@@ -1,58 +1,66 @@
 var game = new Phaser.Game(640, 400, Phaser.AUTO, '', {preload: preload, create:create, update: update});
 
+// loading assets
 function preload() {
     game.load.image('background', "assets/Maps/Scene.png");
     game.load.spritesheet('John', "assets/Characters/John_Cutingem.png", 37, 55);
 }
+
+// global variables
 var player;
 var cursors;
 
+// Initialization
 function create() {
+    // adding physics for controlling characters
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
+    // add background sprite
     var background = game.add.sprite(0, 0,'background');
     background.scale.setTo(2);
-    //game.add.sprite(100, 100, 'John');
 
+    // add player sprite
     player = game.add.sprite(37, 37, 'John');
     player.scale.setTo(2);
     player.animations.add('idle', range(12), 6, true);
-
+    // add physics to it
     game.physics.arcade.enable(player);
-
-    //player.body.gravity.y = 0;
-    //player.body.gravity.x = 0;
-
     player.enableBody = true;
 
+    // initialize control with cursor keys
     cursors = game.input.keyboard.createCursorKeys();
 }
 
+// on update running  function
 function update() {
+    // stop player
     player.body.velocity.x = 0;
     player.body.velocity.y = 0;
     player.animations.play('idle');
 
-    if (cursors.left.isDown)
+    // checking pushed keys
+    // when some navigation keys are pushed, go player to some direction
+    if (cursors.left.isDown || game.input.keyboard.isDown(Phaser.KeyCode.A))
     {
         player.body.velocity.x = -100;
     }
-    if (cursors.right.isDown)
+    if (cursors.right.isDown || game.input.keyboard.isDown(Phaser.KeyCode.D))
     {
         player.body.velocity.x = 100;
     }
-    if (cursors.down.isDown)
+    if (cursors.down.isDown || game.input.keyboard.isDown(Phaser.KeyCode.S))
     {
         player.body.velocity.y = 100;
     }
-    if (cursors.up.isDown)
+    if (cursors.up.isDown || game.input.keyboard.isDown(Phaser.KeyCode.W))
     {
         player.body.velocity.y = -100;
     }
 
 }
 
+// function for getting [0..x] array
 function range(x)
 {
-    return Array.apply(null, Array(x)).map(function(_,i) {return i;})
+    return Array.apply(null, Array(x)).map(function(_,i) {return i;});
 }
