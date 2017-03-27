d5a771b0ffe01cee0e1c9276664efe14106206e8
Stanislaw Babkin <stalissss@outlook.com>
2017-03-28 02:15:56 +0300

    Some improves + comments

--- Game/js/app.js
+++ Game/js/app.js
@@ -2,6 +2,7 @@
  * Created by Stalis on 07.03.2017.
  */
 /// <reference path="phaser.d.ts"/>
+// Default game state class
 var Quest = (function () {
     function Quest() {
         this.game = new Phaser.Game(640, 400, Phaser.AUTO, 'content', {
@@ -19,22 +20,54 @@ var Quest = (function () {
     };
     return Quest;
 }());
+// Base states of parts of char
+var OState;
+(function (OState) {
+    OState[OState["Default"] = 0] = "Default";
+    OState[OState["Light"] = 1] = "Light";
+    OState[OState["Medium"] = 2] = "Medium";
+    OState[OState["Hard"] = 3] = "Hard";
+    OState[OState["Out"] = 4] = "Out";
+})(OState || (OState = {}));
+// Struct w/ states of char
+var CharacterState = (function () {
+    function CharacterState() {
+        this.arms = OState.Default;
+        this.head = OState.Default;
+        this.legs = OState.Default;
+        this.body = OState.Default;
+        this.common = OState.Default;
+        this.radiation = OState.Default;
+        this.acid = OState.Default;
+    }
+    return CharacterState;
+}());
+// Class w/ Role Playing part of char
+var RPChar = (function () {
+    function RPChar() {
+    }
+    return RPChar;
+}());
+// Base class for items
 var Item = (function () {
     function Item() {
     }
     return Item;
 }());
+// Base class of character
 var Character = (function () {
     function Character(name) {
         this.name = name;
     }
     return Character;
 }());
+// Base class for HUD
 var HUD = (function () {
     function HUD() {
     }
     return HUD;
 }());
+// Main function
 window.onload = function () {
     var game = new Quest();
 };
--- Game/typescript/app.ts
+++ Game/typescript/app.ts
@@ -2,6 +2,8 @@
  * Created by Stalis on 07.03.2017.
  */
 /// <reference path="phaser.d.ts"/>
+
+// Default game state class
 class Quest
 {
     constructor()
@@ -29,25 +31,59 @@ class Quest
     }
 }
 
+// Base states of parts of char
+enum OState 
+{
+    Default = 0,
+    Light,
+    Medium,
+    Hard,
+    Out
+}
+
+// Struct w/ states of char
+class CharacterState
+{
+    arms:       OState = OState.Default;
+    head:       OState = OState.Default;
+    legs:       OState = OState.Default;
+    body:       OState = OState.Default;
+    common:     OState = OState.Default;
+    radiation:  OState = OState.Default;
+    acid:       OState = OState.Default;
+}
+
+// Class w/ Role Playing part of char
+class RPChar
+{
+
+}
+
+// Base class for items
 class Item
 {
 
 }
 
+// Base class of character
 class Character
 {
     name: string;
+    state: CharacterState;
     constructor(name)
     {
         this.name = name;
     }
+    rolePart: RPChar;
 }
 
+// Base class for HUD
 class HUD
 {
 
 }
 
+// Main function
 window.onload = () =>
 {
     var game = new Quest();
